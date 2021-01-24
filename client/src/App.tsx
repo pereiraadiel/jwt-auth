5a36e6308b2bdac932a39e64d08cf0eec393f636
Adiel Pereira <adielp16@gmail.com>
2021-01-24 01:25:40 -0300

    first page created

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Button, Input} from 'reakit';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>JWT Auth</h1>
+      <Input className="input" placeholder="E-mail" type="email"/>
+      <Input className="input" placeholder="Senha" type="password"/>
+      <Button className="button" >Entrar</Button>
+      <a href="/create-account">criar uma nova conta</a>
+      <a href="/forgot-password" className="red">recuperar acesso</a>
     </div>
   );
 }
