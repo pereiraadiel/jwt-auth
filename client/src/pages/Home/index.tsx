6634b7827ada1c5e2996379d8d49436a90b821c4
Adiel Pereira <adielp16@gmail.com>
2021-01-24 21:53:13 -0300

    home page connected to api

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,15 +1,61 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { Button, Input} from 'reakit';
+import auth from '../../services/auth';
+
+interface User {
+  id: string,
+  name: string,
+  description: string,
+  photoUrl: string,
+}
 
 const Home = () => {
+  const history = useHistory();
+  const [user, setUser] = useState<User>()
+  useEffect( () => {
+    auth.getUser().then((data: any) => {
+      setUser(data);
+    }).catch((err: any) => {
+      console.error(err);
+      history.goBack();
+    });
+  }, []); 
+  
+  if(!user) {
+    return (
+      <div className="container">
+        <h1>Carregando...</h1>
+      </div>
+    );
+  }
+    
+      
+  console.log("USUARIO>:>:>",user);
   return (
     <div className="container">
       <h1>JWT Auth</h1>
-      <img src="" alt="foto de perfil"/>
-      <Input className="input" placeholder="Name" type="text"/>
-      <Input className="input" placeholder="Photo URL" type="url"/>
-      <textarea className="input" placeholder="Bio"/>
+      <img src={user?.photoUrl} alt="foto de perfil"/>
+      <Input 
+        className="input" 
+        placeholder="Name" 
+        type="text"
+        defaultValue={user?.name}
+        required
+      />
+      <Input 
+        className="input" 
+        placeholder="Photo URL" 
+        type="url"
+        defaultValue={user?.photoUrl}
+        required
+      />
+      <textarea 
+        className="input" 
+        placeholder="Bio"
+        defaultValue={user?.description}
+        required
+      />
       <Button className="button" >Salvar alterações</Button>
       <Link to="/login" className="red">fazer logout</Link>
     </div>
--- client/src/services/auth.tsx
+++ client/src/services/auth.tsx
@@ -1,7 +1,13 @@
 import api from './api';
 
+// interface jwtAuthUser {
+//   id: string,
+//   accessToken: string,
+//   refreshToken: string,
+// }
+
 const isAuthenticated = () => {
-  const token = localStorage.getItem('jwtAuth-user');
+  const token = localStorage.getItem('jwtAuth-user-accessToken');
   if(token) return true;
   return false;
 }
@@ -13,7 +19,9 @@ const authenticate = (email: string, password: string) => {
   })
   .then((response: any) => {
     if(response.data.accessToken){
-      localStorage.setItem("jwtAuth-user", JSON.stringify(response.data));
+      localStorage.setItem("jwtAuth-user-accessToken", JSON.stringify(response.data.accessToken));
+      localStorage.setItem("jwtAuth-user-refreshToken", JSON.stringify(response.data.refreshToken));
+      localStorage.setItem("jwtAuth-user-id", JSON.stringify(response.data.id));
     }
     return response.data;
   })
@@ -22,7 +30,33 @@ const authenticate = (email: string, password: string) => {
   });
 }
 
+const getUser = () => {
+  const id = localStorage.getItem("jwtAuth-user-id")?.replaceAll("\"", '');
+  const accessToken = localStorage.getItem("jwtAuth-user-accessToken")?.replaceAll("\"", '');
+  const refreshToken = localStorage.getItem("jwtAuth-user-refreshToken")?.replaceAll("\"", '');
+  
+  const jwtAuth_user = {
+    id,
+    accessToken,
+    refreshToken
+  }
+
+  if(!jwtAuth_user) return {};
+
+  console.log(jwtAuth_user);
+  
+  return api.get(`/users/${jwtAuth_user.id}`)
+    .then((response: any) => {
+      // console.log(response.data);
+      return response.data;
+    }).catch((err: any) => {
+      return {}
+    });
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   isAuthenticated,
-  authenticate
+  authenticate,
+  getUser
 }
